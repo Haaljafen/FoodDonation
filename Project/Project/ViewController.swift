89c40ef4297e4b2aa13ae20c4eb611ecdeeb4c74
haaljafen <hajaraljafen06@gmail.com>
2025-11-15 22:07:25 +0300

    project is created

---
+++ Project/Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project
+//
+//  Created by Hajar on 15/11/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Project/ProjectTests/ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectTests.swift
+//  ProjectTests
+//
+//  Created by Hajar on 15/11/2025.
+//
+
+import Testing
+@testable import Project
+
+struct ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
